632fb78f2e244ace31fe281cba9353df9d1de8a0
Bernardo Esbérard <bernardo.dsn.erp@alterdata.com.br>
2017-02-06 11:07:30 -0200

    configures opencover.

--- test.cmd
+++ test.cmd
@@ -1,4 +1,8 @@
+nuget install NUnit.Runners -Version 3.6.0 -OutputDirectory testrunner
+nuget install NUnit.Framework -OutputDirectory testrunner
+nuget install OpenCover -Version 4.6.519 -OutputDirectory coverage
 nuget install coveralls.net -Version 0.6.0 -OutputDirectory coverage
-.\coverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ./test/NetCore.Simple.ObjectIdentifier.Tests" -filter:"+[*]* -[*.Test]*" -register:user -output:"coverage.xml"
+dotnet restore .\test\NetCore.Simple.ObjectIdentifier.Tests\
+dotnet build .\test\NetCore.Simple.ObjectIdentifier.Tests\
+.\coverage\OpenCover.4.6.519\tools\Opencover.console.exe -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test "".\test\NetCore.Simple.ObjectIdentifier.Tests"" -f netcoreapp1.0" -returntargetcode -output:"coverage.xml" -oldStyle
 .\coverage\coveralls.net.0.6.0\tools\csmacnz.Coveralls.exe --opencover -i .\coverage.xml
-dir
