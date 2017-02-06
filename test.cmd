9b6c873fdc06e97e1518d0680e71982ce415d47d
Bernardo Esbérard <bernardbr@gmail.com>
2017-02-05 22:28:56 -0200

    Configures test coverage.

---
+++ test.cmd
@@ -0,0 +1,3 @@
+nuget install coveralls.net -Version 0.7.0 -OutputDirectory coverage
+.\coverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:dotnet test -targetargs:"test\NetCore.Simple.ObjectIdentifier.Tests" -filter:"+[*]* -[*.Test]*" -register:user
+.\coverage\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
