6d4a3db3e3a058f1bb7de31a373636711dec8f0d
Bernardo Esbérard <bernardbr@gmail.com>
2017-02-05 22:44:38 -0200

    Adjust test.cmd

--- test.cmd
+++ test.cmd
@@ -1,5 +1,4 @@
 nuget install coveralls.net -Version 0.6.0 -OutputDirectory coverage
-.\coverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:dotnet test -targetargs:"test\NetCore.Simple.ObjectIdentifier.Tests" -filter:"+[*]* -[*.Test]*" -register:user
-.\coverage\coveralls.net.0.6.0\tools\csmacnz.Coveralls.exe --opencover -i .\TestResult.xml
-cls
+.\coverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test ./test/NetCore.Simple.ObjectIdentifier.Tests" -filter:"+[*]* -[*.Test]*" -register:user -output:"coverage.xml"
+.\coverage\coveralls.net.0.6.0\tools\csmacnz.Coveralls.exe --opencover -i .\coverage.xml
 dir
